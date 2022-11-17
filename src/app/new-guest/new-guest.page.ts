3b7ee8fecc121c412146395295155bc9e0d182ea
JUPACAMPOSCA <jupacamposca@ittepic.edu.mx>
2022-11-17 15:28:59 -0700

    Datos estaticos

--- src/app/new-guest/new-guest.page.ts
+++ src/app/new-guest/new-guest.page.ts
@@ -1,6 +1,7 @@
 import { PersonService } from '../services/person.service';
 import { Person } from './../models/person';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-guest',
@@ -9,12 +10,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewGuestPage implements OnInit {
 
-  public person: Person;
+  public person!: Person;
+  public myForm!: FormGroup;
+  public validationMessage!: Object;
 
   constructor(
-    private personService: PersonService
-  ) {
-    this.person = {
+    private personService: PersonService,
+    private fb:FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.myForm = this.fb.group(
+      {
+        name: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.pattern('^[a-zÑñA-Z]+[a-zÑñA-Z ]*$')
+          ])
+        ],
+        phone: [
+          '', 
+          Validators.compose([
+            Validators.required, 
+            Validators.minLength(10), 
+            Validators.maxLength(10), 
+            Validators.pattern('^[0-9]+$')
+          ])
+        ],
+        fecha: [
+          '',
+          Validators.required
+        ]
+      }
+    );
+
+    this.validationMessage = {
+      phone: [
+        {
+          type: 'required',
+          message: 'Número de telefono obligatorio'
+        },
+        {
+          type: 'minlength',
+          message: 'El numero de telefono debe ser de 10 dígitos'
+        },
+        {
+          type: 'maxlength',
+          message: 'El numero de telefono debe ser de 10 dígitos'
+        },
+        {
+          type: 'pattern',
+          message: 'El numero de telefono esta mal formado'
+        }
+      ],
+      name: [
+        {
+          type: 'required',
+          message: 'Nombre oblogatorio'
+        },
+        {
+          type: 'pattern',
+          message: 'El nombre esta mal formado'
+        }
+      ],
+      fecha: [
+        {
+          type: 'required',
+          message: 'Curp obligatoria'
+        }
+      ]
+    }
+  }
+
+  public addPerson() {
+    this.personService.addPerson(this.person);
+    /* this.person = {
       id: 0,
       name: '',
       phone: '',
@@ -22,25 +93,7 @@ export class NewGuestPage implements OnInit {
       fechaFin: '',
       habitación: '',
       tipo: ''
-    }
-  }
-
-  ngOnInit() {
-  }
-
-  public addPerson() {
-    if (this.person.name !== '' && this.person.phone !== '' && this.person.fechaInicio !== null && this.person.fechaFin !== null && this.person.habitación !== '') {
-      this.personService.addPerson(this.person);
-      this.person = {
-        id: 0,
-        name: '',
-        phone: '',
-        fechaInicio: '',
-        fechaFin: '',
-        habitación: '',
-        tipo: ''
-      }
-    }
+    } */
   }
 
 }
--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -15,17 +15,45 @@ export class PersonService {
         id: 1,
         name: 'Juan Pablo Campos',
         phone: '',
-        fechaInicio: "",
+        fechaInicio: '',
         fechaFin: '',
         habitación: '',
         tipo: 'admin'
+      },
+      {
+        id: 2,
+        name: 'Adrian',
+        phone: '3112222222',
+        fechaInicio: '2022-11-17',
+        fechaFin: '2022-11-19',
+        habitación: 'A1',
+        tipo: 'guest'
+      },
+      {
+        id: 3,
+        name: 'Ana',
+        phone: '3112222221',
+        fechaInicio: '2022-11-17',
+        fechaFin: '2022-11-19',
+        habitación: 'A2',
+        tipo: 'guest'
+      },
+      {
+        id: 4,
+        name: 'Polo',
+        phone: '3112422222',
+        fechaInicio: '2022-11-17',
+        fechaFin: '2022-11-19',
+        habitación: 'A3',
+        tipo: 'guest'
       }
     ]
 
   }
 
   public addPerson(persona: Person){
-
+    this.person.push(persona);
+    console.log(this.person)
   }
 
 
