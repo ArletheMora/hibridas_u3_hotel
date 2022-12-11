3725f256710fe02fe7bb153a7cc3b99489c21830
JUPACAMPOSCA <jupacamposca@ittepic.edu.mx>
2022-12-11 14:47:27 -0700

    prueba de camara

--- src/app/camera/camera.page.ts
+++ src/app/camera/camera.page.ts
@@ -38,7 +38,7 @@ export class CameraPage implements OnInit {
       quality: 90,
       source: CameraSource.Prompt,
       width: 600,
-      resultType: CameraResultType.Uri
+      resultType: CameraResultType.DataUrl
     });
     this.selectedImage = image;
     /* if(this.checkPlataformForWeb())  */
